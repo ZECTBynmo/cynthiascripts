d4374dabe173ef559ad31003c4f664910f20d5ea
Mike Vegeto <michael.vegeto@gmail.com>
2019-06-10 12:06:09 -0400

    remote rename

--- src/jewels.js
+++ src/jewels.js
@@ -27,7 +27,6 @@ const getFiles = (dir, subPath=[], all=false) => {
       const dest = `${dir}/${destName}`
 
       if (all || subPath.includes('m1') || subPath.includes('m2') || subPath.includes('m3') || subPath.includes('m4') || subPath.includes('m5')) {
-        fs.renameSync(fullPath, dest)
         files.push(dest)
       }
     }
